32d51389e58d96832d818ecc963913e01105ddcf
Abhishek Gupta <abhishek.m.gupta@slrtce.in>
2026-09-20 22:22:30 +0530

    Time: 124 ms (58.10%) | Memory: 12.4 MB (46.07%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        ans =[]
+
+        for num in nums:
+            count=0
+
+            for x in nums:
+                if x < num:
+                    count+=1
+            ans.append(count)
+
+        return ans
+
+
+       
+        
