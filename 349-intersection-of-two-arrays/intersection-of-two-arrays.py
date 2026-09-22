2b1443ae14f2a4daa1caa0a23c795fc58d33e1cb
Abhishek Gupta <abhishek.m.gupta@slrtce.in>
2026-09-22 22:18:58 +0530

    Time: 44 ms (7.34%) | Memory: 12.5 MB (67.66%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+
+        ans =[]
+
+        for i in nums1:
+            for j in nums2:
+                if i == j and i not in ans:
+                    ans.append(i)
+                    break
+
+        return ans
+
+                
+                 
+                    
+
+
+        
+        
