761f261e612e54d9d93abcb608ff3ae408a657cd
Abhishek Gupta <abhishek.m.gupta@slrtce.in>
2026-09-26 12:12:06 +0530

    Time: 15 ms (71.51%) | Memory: 13.5 MB (32.96%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s = s.lower()
+        s = ''.join(ch for ch in s if ch.isalnum())
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
